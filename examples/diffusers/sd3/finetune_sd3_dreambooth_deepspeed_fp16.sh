430e67282c6a24d02779a089a892b40194bd4d2d
J石页 <jiangshuo9@h-partners.com>
2024-12-03 06:55:35 +0000

    !417 【bugfix】requirement及FPS打印修改 Merge pull request !417 from J石页/1.0.RC3

--- examples/diffusers/sd3/finetune_sd3_dreambooth_deepspeed_fp16.sh
+++ examples/diffusers/sd3/finetune_sd3_dreambooth_deepspeed_fp16.sh
@@ -87,8 +87,14 @@ echo "------------------ Final result ------------------"
 #输出性能FPS，需要模型审视修改
 AverageIts=$(grep -o "[0-9.]*s/it" ${output_path}/train_${mixed_precision}_sd3_dreambooth_deepspeed.log | sed -n '100,199p' | awk '{a+=$1}END{print a/NR}')
 
-echo "Average s/it: ${AverageIts}"
-FPS=$(awk 'BEGIN{printf "%.2f\n",'${batch_size}'*'${num_processors}'/'${AverageIts}'}')
+if [ "$(echo "$AverageIts == 0")" ]; then
+  AverageIts=$(grep -o "[0-9.]*it/s" ${output_path}/train_${mixed_precision}_sd3_dreambooth_deepspeed.log | sed -n '100,199p' | awk '{a+=$1}END{print a/NR}')
+  echo "Average it/s: ${AverageIts}"
+  FPS=$(awk 'BEGIN{printf "%.2f\n",'${batch_size}'*'${num_processors}'*'${AverageIts}'}')
+else
+  echo "Average s/it: ${AverageIts}"
+  FPS=$(awk 'BEGIN{printf "%.2f\n",'${batch_size}'*'${num_processors}'/'${AverageIts}'}')
+fi
 
 #获取性能数据，不需要修改
 # - 吞吐量
