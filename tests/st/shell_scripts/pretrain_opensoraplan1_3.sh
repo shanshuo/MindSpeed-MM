11c6afaf2c5411008f4a302e675de305178ab68b
zhushixiang <1318709014@qq.com>
2024-12-04 12:28:19 +0000

    !426 【测试】添加open-sora-plan 1.3 st用例看护 Merge pull request !426 from zhushixiang/master

---
+++ tests/st/shell_scripts/pretrain_opensoraplan1_3.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+source /usr/local/Ascend/ascend-toolkit/set_env.sh
+export CUDA_DEVICE_MAX_CONNECTIONS=1
+export ASCEND_SLOG_PRINT_TO_STDOUT=0
+export ASCEND_GLOBAL_LOG_LEVEL=3
+export TASK_QUEUE_ENABLE=1
+export COMBINED_ENABLE=1
+export CPU_AFFINITY_CONF=1
+export HCCL_CONNECT_TIMEOUT=1200
+
+GPUS_PER_NODE=4
+MASTER_ADDR=localhost
+MASTER_PORT=29505
+NNODES=1
+NODE_RANK=0
+WORLD_SIZE=$(($GPUS_PER_NODE*$NNODES))
+
+TP=2
+PP=1
+CP=2
+MBS=1
+GBS=$(($WORLD_SIZE*$MBS/$CP/$TP))
+
+BASEPATH=$(cd `dirname $0`; cd ../../../; pwd)
+
+MM_DATA="$BASEPATH/tests/st/run_configs/pretrain_opensoraplan1_3/data.json"
+MM_MODEL="$BASEPATH/tests/st/run_configs/pretrain_opensoraplan1_3/model.json"
+MM_TOOL="$BASEPATH/mindspeed_mm/tools/tools.json"
+LOAD_PATH="/home/ci_resource/models/opensoraplan1_3"
+
+DISTRIBUTED_ARGS="
+    --nproc_per_node $GPUS_PER_NODE \
+    --nnodes $NNODES \
+    --node_rank $NODE_RANK \
+    --master_addr $MASTER_ADDR \
+    --master_port $MASTER_PORT
+"
+
+GPT_ARGS="
+    --tensor-model-parallel-size ${TP} \
+    --pipeline-model-parallel-size ${PP} \
+    --context-parallel-size ${CP} \
+    --micro-batch-size ${MBS} \
+    --global-batch-size ${GBS} \
+    --num-layers 32 \
+    --hidden-size 2304 \
+    --num-attention-heads 24 \
+    --seq-length 1024 \
+    --max-position-embeddings 1024 \
+    --attention-dropout 0.0 \
+    --hidden-dropout 0.0 \
+    --tokenizer-type NullTokenizer \
+    --vocab-size 0 \
+    --position-embedding-type rope \
+    --rotary-base 500000 \
+    --swiglu \
+    --no-masked-softmax-fusion \
+    --lr 1e-5 \
+    --min-lr 1e-5 \
+    --adam-beta1 0.9 \
+    --adam-beta2 0.999 \
+    --adam-eps 1e-15 \
+    --lr-decay-style constant \
+    --weight-decay 1e-2 \
+    --lr-warmup-init 1e-5 \
+    --lr-warmup-iters 0 \
+    --clip-grad 1.0 \
+    --train-iters 3 \
+    --no-gradient-accumulation-fusion \
+    --no-load-optim \
+    --no-load-rng \
+    --no-save-optim \
+    --no-save-rng \
+    --bf16 \
+    --use-distributed-optimizer \
+    --sequence-parallel \
+"
+
+MM_ARGS="
+    --mm-data $MM_DATA \
+    --mm-model $MM_MODEL \
+    --mm-tool $MM_TOOL
+"
+
+OUTPUT_ARGS="
+    --log-interval 1 \
+    --save-interval 10000 \
+    --eval-interval 10000 \
+    --eval-iters 10 \
+"
+
+torchrun $DISTRIBUTED_ARGS $BASEPATH/pretrain_sora.py \
+    $GPT_ARGS \
+    $MM_ARGS \
+    $OUTPUT_ARGS \
+    --distributed-backend nccl
