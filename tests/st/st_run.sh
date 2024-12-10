8d0e622c6974933834ad1be7ee03086aa5430ec7
陆劲夫 <979323654@qq.com>
2024-12-10 01:18:28 +0000

    !431 【测试】添加ST各用例执行时间打印 Merge pull request !431 from 陆劲夫/master

--- tests/st/st_run.sh
+++ tests/st/st_run.sh
@@ -21,12 +21,14 @@ rm -rf $GENERATE_JSON_DIR/*
 
 
 # step 2: running scripts and execute `test_ci_st.py`
+declare -A TEST_CASE_TIMES
 for test_case in "$SHELL_SCRIPTS_DIR"/*.sh; do
     file_name=$(basename "${test_case}")
     echo "Running $file_name..."
     file_name_prefix=$(basename "${file_name%.*}")
     echo "$file_name_prefix"
 
+    START_TIME=$(date +%s)
     # create empty json file to receive the result parsered from log
     touch "$GENERATE_JSON_DIR/$file_name_prefix.json"
 
@@ -37,12 +39,24 @@ for test_case in "$SHELL_SCRIPTS_DIR"/*.sh; do
         echo "Script has failed. Exit!"
         exit 1
     fi
+
+    END_TIME=$(date +%s)
+    ELAPSED_TIME=$((END_TIME-START_TIME))
+    MINUTES=$((ELAPSED_TIME / 60))
+    SECONDS=$((ELAPSED_TIME % 60))
+    TEST_CASE_TIMES["$file_name"]="$MINUTES m $SECONDS s"
+
+    if [ "$MINUTES" -gt 0 ]; then
+        echo "$(printf '*%.0s' {1..20}) Execution Time for $file_name: *${MINUTES}m ${SECONDS}s* $(printf '*%.0s' {1..20})"
+    else
+        echo "$(printf '*%.0s' {1..20}) Execution Time for $file_name: *${SECONDS}s* $(printf '*%.0s' {1..20})"
+    fi
     # begin to execute the logic of compare
     pytest -x $BASE_DIR/test_tools/test_ci_st.py \
         --baseline-json $BASELINE_DIR/$file_name_prefix.json \
         --generate-log $GENERATE_LOG_DIR/$file_name_prefix.log \
         --generate-json $GENERATE_JSON_DIR/$file_name_prefix.json
-    
+
     PYTEST_EXITCODE=$?
     echo $PYTEST_EXITCODE
     if [ $PYTEST_EXITCODE -ne 0 ]; then
@@ -53,4 +67,12 @@ for test_case in "$SHELL_SCRIPTS_DIR"/*.sh; do
     fi
 
 done
+
+echo "$(printf '*%.0s' {1..40})"
+echo "* Summary of Execution Times for All Test Cases *"
+echo "$(printf '*%.0s' {1..40})"
+for file_name in "${!TEST_CASE_TIMES[@]}"; do
+    echo "* Execution Time for $file_name: ${TEST_CASE_TIMES[$file_name]} *"
+done
+echo "$(printf '*%.0s' {1..40})"
 
