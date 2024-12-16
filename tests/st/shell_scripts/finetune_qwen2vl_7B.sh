a64c9eb4107a5bd4891004a875169a3f6945558e
zhangxianshen <zhangjunyi8@huawei.com>
2024-12-16 11:22:41 +0000

    !512 【特性】qwen2-vl st Merge pull request !512 from zhangxianshen/master

---
+++ tests/st/shell_scripts/finetune_qwen2vl_7B.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+export CUDA_DEVICE_MAX_CONNECTIONS=1
+export ASCEND_SLOG_PRINT_TO_STDOUT=0
+export ASCEND_GLOBAL_LOG_LEVEL=3
+export TASK_QUEUE_ENABLE=2
+export COMBINED_ENABLE=1
+export CPU_AFFINITY_CONF=1
+export HCCL_CONNECT_TIMEOUT=1200
+export NPU_ASD_ENABLE=0
+export ASCEND_LAUNCH_BLOCKING=0
+export HOST_CACHE_CAPACITY=20
+export ACLNN_CACHE_LIMIT=100000
+
+NPUS_PER_NODE=8
+MASTER_ADDR=localhost
+MASTER_PORT=6002
+NNODES=1
+NODE_RANK=0
+WORLD_SIZE=$(($NPUS_PER_NODE*$NNODES))
+
+BASEPATH=$(cd `dirname $0`; cd ../../../; pwd)
+
+MM_DATA="$BASEPATH/tests/st/run_configs/finetune_qwen2vl_7B/data_7B.json"
+MM_MODEL="$BASEPATH/tests/st/run_configs/finetune_qwen2vl_7B/model_7B.json"
+MM_TOOL="$BASEPATH/mindspeed_mm/tools/tools.json"
+# LOAD_PATH="/mnt/disk0/ci_resource/models/qwen2vl_7b/qwen2vl_7b_ckpt"
+
+TP=1
+PP=4
+CP=1
+SEQ_LEN=1024
+MBS=1
+GRAD_ACC_STEP=2
+DP=$(($WORLD_SIZE/$TP/$PP/$CP))
+GBS=$(($MBS*$GRAD_ACC_STEP*$DP))
+
+DISTRIBUTED_ARGS="
+    --nproc_per_node $NPUS_PER_NODE \
+    --nnodes $NNODES \
+    --node_rank $NODE_RANK \
+    --master_addr $MASTER_ADDR \
+    --master_port $MASTER_PORT
+"
+
+GPT_ARGS="
+    --use-mcore-models \
+    --tensor-model-parallel-size ${TP} \
+    --pipeline-model-parallel-size ${PP} \
+    --micro-batch-size ${MBS} \
+    --global-batch-size ${GBS} \
+    --num-layers 1 \
+    --hidden-size 1 \
+    --ffn-hidden-size 1 \
+    --num-attention-heads 1 \
+    --tokenizer-type NullTokenizer \
+    --vocab-size 1 \
+    --seq-length 1 \
+    --max-position-embeddings 1 \
+    --make-vocab-size-divisible-by 1 \
+    --init-method-std 0.01 \
+    --normalization RMSNorm \
+    --use-fused-rmsnorm \
+    --swiglu \
+    --use-fused-swiglu \
+    --lr 1.0e-5 \
+    --lr-decay-style cosine \
+    --weight-decay 0 \
+    --train-iters 3 \
+    --lr-warmup-fraction 0.1 \
+    --clip-grad 0.0 \
+    --adam-beta1 0.9 \
+    --adam-beta2 0.999 \
+    --no-gradient-accumulation-fusion \
+    --no-load-optim \
+    --no-load-rng \
+    --no-save-optim \
+    --no-save-rng \
+    --seed 42 \
+    --bf16 \
+    --variable-seq-lengths \
+    --use-distributed-optimizer \
+    --enable-one-logger
+"
+
+MM_ARGS="
+    --mm-data $MM_DATA \
+    --mm-model $MM_MODEL \
+    --mm-tool $MM_TOOL
+"
+
+OUTPUT_ARGS="
+    --log-interval 1 \
+    --save-interval 5000 \
+    --eval-interval 5000 \
+    --eval-iters 5000 \
+"
+
+
+torchrun $DISTRIBUTED_ARGS \
+    $BASEPATH/pretrain_qwen2vl.py \
+    $GPT_ARGS \
+    $MM_ARGS \
+    $OUTPUT_ARGS \
+    --distributed-backend nccl
