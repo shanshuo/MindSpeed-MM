84626fa661644aa6c7432e38966634e0af9957d2
zhanggaolu <zhanggaolu122@163.com>
2024-12-24 02:14:36 +0000

    !545 【特性】CogVideox I2V 1.5 功能适配 Merge pull request !545 from zhanggaolu/master

--- examples/cogvideox/i2v_1.0/pretrain_cogvideox_i2v.sh
+++ examples/cogvideox/i2v_1.0/pretrain_cogvideox_i2v.sh
@@ -7,7 +7,7 @@ export COMBINED_ENABLE=1
 export CPU_AFFINITY_CONF=1
 export HCCL_CONNECT_TIMEOUT=1200
 export CUDA_DEVICE_MAX_CONNECTIONS=1
-
+export PYTORCH_NPU_ALLOC_CONF=expandable_segments:True
 GPUS_PER_NODE=8
 MASTER_ADDR=localhost
 MASTER_PORT=29505
@@ -21,8 +21,8 @@ CP=1
 MBS=1
 GBS=$(($WORLD_SIZE*$MBS/$CP))
 
-MM_DATA="./examples/cogvideox/data.json"
-MM_MODEL="./examples/cogvideox/model_cogvideox_i2v.json"
+MM_DATA="./examples/cogvideox/i2v_1.0/data.json"
+MM_MODEL="./examples/cogvideox/i2v_1.0/model_cogvideox_i2v.json"
 MM_TOOL="./mindspeed_mm/tools/tools.json"
 
 DISTRIBUTED_ARGS="
@@ -100,5 +100,5 @@ torchrun $DISTRIBUTED_ARGS pretrain_sora.py \
 
 chmod 440 logs/train_${logfile}.log
 STEP_TIME=`grep "elapsed time per iteration" logs/train_${logfile}.log | awk -F ':' '{print$5}' | awk -F '|' '{print$1}' | head -n 200 | tail -n 100 | awk '{sum+=$1} END {if (NR != 0) printf("%.1f",sum/NR)}'`
-FPS=`awk 'BEGIN{printf "%.3f\n", '${GBS}'*1000/'${STEP_TIME}'}'`
-echo "Elapsed Time Per iteration: $STEP_TIME, Average FPS: $FPS"
+SPS=`awk 'BEGIN{printf "%.3f\n", '${GBS}'*1000/'${STEP_TIME}'}'`
+echo "Elapsed Time Per iteration: $STEP_TIME, Average Samples per Second: $SPS"
