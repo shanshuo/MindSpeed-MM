e06b78f78eb2449d7ea3541a900bdb284c0a30af
liyingxuan <liyingxuan3@huawei.com>
2025-01-18 02:59:21 +0000

    !617 【bugfix】删除repeat_interleave的patch Merge pull request !617 from liyingxuan/master

--- mindspeed_mm/patchs/__init__.py
+++ mindspeed_mm/patchs/__init__.py
@@ -3,13 +3,4 @@ import torch
 from .patch_manager import PatchesManager
 
 
-def repeat_interleave(inputs, repeats, dim):
-    shape = inputs.shape
-    new_shape = shape[:dim + 1] + (repeats,) + shape[dim + 1:]
-    out_shape = shape[:dim] + (shape[dim] * repeats,) + shape[dim + 1:]
-    return inputs.unsqueeze(dim + 1).expand(new_shape).reshape(out_shape)
-
-torch.Tensor.repeat_interleave = repeat_interleave
-
-
 __all__ = ["PatchesManager"]
