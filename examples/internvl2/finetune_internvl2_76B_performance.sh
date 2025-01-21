d966bc13101c2a72dd6fb09d430c45569f22ee9d
chenhaihui <1020505070@qq.com>
2025-01-21 13:47:51 +0000

    !634 【特性】新增InternVL2 76B用于A3性能的启动脚本 Merge pull request !634 from chenhaihui/master

---
+++ examples/internvl2/finetune_internvl2_76B_performance.sh
@@ -0,0 +1,123 @@
+#!/bin/bash
+source /usr/local/Ascend/ascend-toolkit/set_env.sh
+export ASCEND_SLOG_PRINT_TO_STDOUT=0
+export ASCEND_GLOBAL_LOG_LEVEL=3
+export TASK_QUEUE_ENABLE=2
+export COMBINED_ENABLE=1
+export CPU_AFFINITY_CONF=1
+export HCCL_CONNECT_TIMEOUT=1200
+export CUDA_DEVICE_MAX_CONNECTIONS=1
+export ACLNN_CACHE_LIMIT=100000
+
+NPUS_PER_NODE=16
+MASTER_PORT=6000
+HOSTFILE='./hostfile'
+NODEADDR=$(hostname -I | awk -F " " '{print$1}')
+NODE_RANK=$(awk '{ranks[$1]=(FNR-1);}END{print ranks["'$NODEADDR'"];}' $HOSTFILE)
+NNODES=$(wc -l $HOSTFILE)
+MASTER_ADDR=$(head -n 1 $HOSTFILE | awk '{print $1;}')
+
+WORLD_SIZE=$(($NPUS_PER_NODE*$NNODES))
+
+MBS=1
+GRAD_ACC_STEP=256
+TP=1
+PP=16
+CP=1
+DP=$(($WORLD_SIZE/$TP/$PP/$CP))
+GBS=$(($MBS*$GRAD_ACC_STEP*$DP))
+
+MM_DATA="./examples/internvl2/data_76B.json"
+MM_MODEL="./examples/internvl2/model_76B_performance.json"
+MM_TOOL="./mindspeed_mm/tools/tools.json"
+LOAD_PATH="InternVL2-76B_pp16"
+SAVE_PATH="save_dir"
+
+MM_ARGS="
+    --mm-data ${MM_DATA} \
+    --mm-model ${MM_MODEL} \
+    --mm-tool ${MM_TOOL}
+"
+
+DISTRIBUTED_ARGS="
+    --nproc_per_node $NPUS_PER_NODE \
+    --nnodes $NNODES \
+    --node_rank $NODE_RANK \
+    --master_addr $MASTER_ADDR \
+    --master_port $MASTER_PORT
+"
+
+GPT_ARGS="
+    --tensor-model-parallel-size ${TP} \
+    --pipeline-model-parallel-size ${PP} \
+    --context-parallel-size ${CP} \
+    --micro-batch-size ${MBS} \
+    --global-batch-size ${GBS} \
+    --num-layers 80 \
+    --hidden-size 8192 \
+    --num-attention-heads 16 \
+    --seq-length 8192 \
+    --max-position-embeddings 8192 \
+    --attention-dropout 0.0 \
+    --hidden-dropout 0.0 \
+    --tokenizer-type NullTokenizer \
+    --vocab-size 128258 \
+    --position-embedding-type rope \
+    --rotary-base 100000 \
+    --swiglu \
+    --lr 1e-5 \
+    --min-lr 0.0 \
+    --train-iters 5000 \
+    --lr-decay-style cosine \
+    --weight-decay 0.01 \
+    --clip-grad 1.0 \
+    --adam-beta1 0.9 \
+    --adam-beta2 0.999 \
+    --no-gradient-accumulation-fusion \
+    --bf16 \
+    --use-distributed-optimizer \
+    --use-flash-attn \
+    --variable-seq-lengths \
+    --load $LOAD_PATH \
+    --no-load-optim \
+    --no-load-rng \
+    --no-save-optim \
+    --no-save-rng \
+    --num-workers 8 \
+    --overlap-grad-reduce \
+    --overlap-param-gather \
+    --normalization RMSNorm \
+    --use-fused-rmsnorm \
+    --use-fused-rotary-pos-emb \
+    --use-fused-swiglu \
+"
+
+OUTPUT_ARGS="
+    --log-interval 1 \
+    --save-interval 5000 \
+    --eval-interval 5000 \
+    --eval-iters 5000 \
+    --save $SAVE_PATH \
+"
+
+logfile=$(date +%Y%m%d)_$(date +%H%M%S)
+mkdir -p logs
+torchrun $DISTRIBUTED_ARGS \
+    pretrain_internvl.py \
+    $GPT_ARGS \
+    $MM_ARGS \
+    $OUTPUT_ARGS \
+    --distributed-backend nccl \
+    2>&1 | tee logs/train_${logfile}.log
+chmod 440 logs/train_${logfile}.log
+chmod -R 640 $SAVE_PATH
+STEP_TIME=`grep "elapsed time per iteration" logs/train_${logfile}.log | awk -F ':' '{print$5}' | awk -F '|' '{print$1}' | head -n 150 | tail -n 100 | awk '{sum+=$1} END {if (NR != 0) printf("%.1f",sum/NR)}'`
+SAMPLES_PER_SECOND=`awk 'BEGIN{printf "%.3f\n", '${GBS}'*1000/'${STEP_TIME}'}'`
+echo "Elapsed Time Per iteration: $STEP_TIME"
+echo "Average Samples per Second: $SAMPLES_PER_SECOND"
+LOG_TOKENS_PER_SECOND=`grep "tokens per sample" logs/train_${logfile}.log`
+if [ "$LOG_TOKENS_PER_SECOND" ]; then
+    AVERAGE_TOKENS=`grep "tokens per sample" logs/train_${logfile}.log | awk -F 'tokens per sample:' '{print$2}' | awk -F '|' '{print$1}' | head -n 150 | tail -n 100 | awk '{sum+=$1} END {if (NR != 0) printf("%.1f",sum/NR)}'`
+    TOKENS_PER_SECOND=`awk 'BEGIN{printf "%.3f\n", '${SAMPLES_PER_SECOND}'*'${AVERAGE_TOKENS}'}'`
+    echo "Consumed Tokens per Second: $TOKENS_PER_SECOND"
+fi
