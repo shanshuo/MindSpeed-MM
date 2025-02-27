f0acc9963a2e7c9d41de3ba6051c1fa0795b61c6
SunnyLee219 <3294305115@qq.com>
2025-02-27 11:30:25 +0000

    !738 修改Ulysses Patches，兼容MindSpeed All2All Merge pull request !738 from SunnyLee219/master

--- mindspeed_mm/patchs/ulysses_patches.py
+++ mindspeed_mm/patchs/ulysses_patches.py
@@ -9,11 +9,14 @@ from torch import Tensor
 from torch.nn import Module 
 from mindspeed.megatron_adaptor import get_mindspeed_args
 from mindspeed.patch_utils import MindSpeedPatchesManager as pm
+try: 
+    from mindspeed.core.context_parallel.unaligned_cp.mapping import all_to_all
+    native_all_to_all = False
+except ImportError:
+    from mindspeed_mm.models.common.communications import all_to_all
+    native_all_to_all = True 
 
 from mindspeed_mm.models.common.communications import cal_split_sizes
-from mindspeed_mm.models.common.communications import all_to_all
-
-
 
 
 class UlyssesContextAttention(torch.nn.Module):
@@ -57,17 +60,24 @@ class UlyssesContextAttention(torch.nn.Module):
         scatter_sizes_value = cal_split_sizes(value.shape[self.scatter_idx], dist.get_world_size(self.spg))
 
         gather_sizes = cal_split_sizes(act_seq_len, dist.get_world_size(self.spg))
-
-        query_layer = all_to_all(query, self.spg, self.scatter_idx, self.gather_idx, scatter_sizes_query, gather_sizes)
-        key_layer = all_to_all(key, self.spg, self.scatter_idx, self.gather_idx, scatter_sizes_key, gather_sizes)
-        value_layer = all_to_all(value, self.spg, self.scatter_idx, self.gather_idx, scatter_sizes_value, gather_sizes)
-
+        if not native_all_to_all:
+            query_layer = all_to_all(query, self.spg, self.scatter_idx, self.gather_idx, act_seq_len)
+            key_layer = all_to_all(key, self.spg, self.scatter_idx, self.gather_idx, act_seq_len)
+            value_layer = all_to_all(value, self.spg, self.scatter_idx, self.gather_idx, act_seq_len)
+        else:
+            query_layer = all_to_all(query, self.spg, self.scatter_idx, self.gather_idx, scatter_sizes_query, gather_sizes)
+            key_layer = all_to_all(key, self.spg, self.scatter_idx, self.gather_idx, scatter_sizes_key, gather_sizes)
+            value_layer = all_to_all(value, self.spg, self.scatter_idx, self.gather_idx, scatter_sizes_value, gather_sizes)
+            
         context_layer = self.local_attn(query_layer, key_layer, value_layer, *args, **kwargs)
         context_shape = context_layer.shape
         context_layer = context_layer.reshape(context_shape[0], context_shape[1],
                                             scatter_sizes_query[dist.get_rank(self.spg)], -1)
         
-        output = all_to_all(context_layer, self.spg, self.gather_idx, self.scatter_idx, gather_sizes, scatter_sizes_query)
+        if not native_all_to_all:
+            output = all_to_all(context_layer, self.spg, self.gather_idx, self.scatter_idx, query.shape[self.scatter_idx])
+        else:
+            output = all_to_all(context_layer, self.spg, self.gather_idx, self.scatter_idx, gather_sizes, scatter_sizes_query)
         output = output.reshape(output.shape[0], output.shape[1], -1)
 
         # out e.g., [s/p::h]
