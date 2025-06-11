4aeaf4dba4b8ccccb43d23d865ea0acc07847137
liyingxuan <liyingxuan3@huawei.com>
2025-06-11 06:47:44 +0000

    !1085 [Test] ST for hunyuanvideo Merge pull request !1085 from liyingxuan/master

---
+++ tests/st/shell_scripts/pretrain_hunyuanvideo_t2v.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+source /usr/local/Ascend/ascend-toolkit/set_env.sh
+# 该变量只用于规避megatron对其校验，对npu无效
+export CUDA_DEVICE_MAX_CONNECTIONS=1
+export ASCEND_SLOG_PRINT_TO_STDOUT=0
+export ASCEND_GLOBAL_LOG_LEVEL=3
+export TASK_QUEUE_ENABLE=1
+export COMBINED_ENABLE=1
+export CPU_AFFINITY_CONF=1
+export HCCL_CONNECT_TIMEOUT=1200
+export PYTORCH_NPU_ALLOC_CONF=expandable_segments:True
+
+NPUS_PER_NODE=4
+MASTER_ADDR=localhost
+MASTER_PORT=29505
+NNODES=1
+NODE_RANK=0
+WORLD_SIZE=$(($NPUS_PER_NODE*$NNODES))
+
+TP=4
+PP=1
+CP=1
+MBS=1
+GBS=$(($WORLD_SIZE*$MBS/$CP/$TP))
+
+BASEPATH=$(cd `dirname $0`; cd ../../../; pwd)
+
+MM_DATA="$BASEPATH/tests/st/run_configs/pretrain_hunyuanvideo_t2v/data.json"
+MM_MODEL="$BASEPATH/tests/st/run_configs/pretrain_hunyuanvideo_t2v/model.json"
+MM_TOOL="$BASEPATH/mindspeed_mm/tools/tools.json"
+
+
+DISTRIBUTED_ARGS="
+    --nproc_per_node $NPUS_PER_NODE \
+    --nnodes $NNODES \
+    --node_rank $NODE_RANK \
+    --master_addr $MASTER_ADDR \
+    --master_port $MASTER_PORT
+"
+
+GPT_ARGS="
+    --tensor-model-parallel-size ${TP} \
+    --pipeline-model-parallel-size ${PP} \
+    --context-parallel-size ${CP} \
+    --micro-batch-size ${MBS} \
+    --global-batch-size ${GBS} \
+    --lr 5e-6 \
+    --min-lr 5e-6 \
+    --adam-beta1 0.9 \
+    --adam-beta2 0.999 \
+    --adam-eps 1e-8 \
+    --lr-decay-style constant \
+    --weight-decay 1e-2 \
+    --lr-warmup-init 0 \
+    --lr-warmup-iters 0 \
+    --clip-grad 1.0 \
+    --train-iters 3 \
+    --no-gradient-accumulation-fusion \
+    --no-load-optim \
+    --no-load-rng \
+    --no-save-optim \
+    --no-save-rng \
+    --bf16 \
+    --recompute-granularity full \
+    --recompute-method block \
+    --recompute-num-layers 6 \
+    --use-distributed-optimizer \
+    --overlap-grad-reduce \
+    --overlap-param-gather \
+    --num-workers 8 \
+"
+
+MM_ARGS="
+    --mm-data $MM_DATA \
+    --mm-model $MM_MODEL \
+    --mm-tool $MM_TOOL
+"
+
+OUTPUT_ARGS="
+    --log-interval 1 \
+    --save-interval 10000 \
+    --eval-interval 10000 \
+    --eval-iters 10 \
+"
+
+torchrun $DISTRIBUTED_ARGS $BASEPATH/pretrain_sora.py \
+    $GPT_ARGS \
+    $MM_ARGS \
+    $OUTPUT_ARGS \
+    --distributed-backend nccl
