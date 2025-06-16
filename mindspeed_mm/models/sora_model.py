!1101 [Bugfix] Change FPS calculation & config fix & dynamic shape & sdxl finetune info Merge pull request !1101 from J石页/master

mindspeed_mm/models/sora_model.py
@@ -151,13 +151,14 @@ class SoRAModel(nn.Module):
                             latents, i2v_results = self.ae.encode(video, **kwargs)
                         else:
                             raise NotImplementedError(f"Task {self.task} is not Implemented!")
-                    kwargs.update({"shape": latents.shape})
 
             # Gather the results after encoding of ae and text_encoder
             if self.enable_encoder_dp or self.interleaved_steps > 1:
                 if self.index == 0:
                     self.init_cache(latents, prompt, video_mask, prompt_mask, i2v_results)
                 latents, prompt, video_mask, prompt_mask, i2v_results = self.get_feature_from_cache()
+            else:
+                kwargs.update({"shape": latents.shape})
 
             if self.task == "i2v" and i2v_results is not None:
                 kwargs.update(i2v_results)