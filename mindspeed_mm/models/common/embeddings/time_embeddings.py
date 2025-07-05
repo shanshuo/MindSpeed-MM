1a35b5d2919360d03f83804f1022e7adaa08d52c
J石页 <jiangshuo9@h-partners.com>
2025-07-05 08:43:50 +0000

    !1225 [Docs] Comment modification Merge pull request !1225 from J石页/master

--- mindspeed_mm/models/common/embeddings/time_embeddings.py
+++ mindspeed_mm/models/common/embeddings/time_embeddings.py
@@ -6,14 +6,6 @@ from einops import rearrange, repeat
 
 
 def timestep_embedding(timesteps, dim, max_period=10000, repeat_only=False, dtype=torch.float32):
-    """
-    Create sinusoidal timestep embeddings.
-    :param timesteps: a 1-D Tensor of N indices, one per batch element.
-                      These may be fractional.
-    :param dim: the dimension of the output.
-    :param max_period: controls the minimum frequency of the embeddings.
-    :return: an [N x dim] Tensor of positional embeddings.
-    """
     if not repeat_only:
         half = dim // 2
         freqs = torch.exp(-math.log(max_period) * torch.arange(start=0, end=half, dtype=torch.float32) / half).to(
